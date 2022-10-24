06d34d77c4386b6bd90ceddbb559e4be3ab0ba07
Kurzov <kurzanbl4@gmail.com>
2022-10-24 17:01:25 +0300

    Добавление noUiSlider

--- js/effects.js
+++ js/effects.js
@@ -2,6 +2,9 @@ import {photoPreview} from './picture-scale.js';
 
 const effectsRadio = document.querySelectorAll('.effects__radio');
 
+const sliderElement = document.querySelector('.effect-level__slider');
+const sliderValue = document.querySelector('.effect-level__value');
+
 effectsRadio.forEach((effect) => {
   effect.addEventListener('change', () => {
     photoPreview.classList = '';
@@ -14,4 +17,23 @@ const resetEffects = () => {
   photoPreview.style.transform = '';
 };
 
+
+//Создание слайдера
+noUiSlider.create(sliderElement, {
+  range: {
+    min: 0,
+    max: 100,
+  },
+  start: 80,
+  step: 1,
+  connect: 'lower',
+});
+
+//Получение значения слайдера
+sliderElement.noUiSlider.on('update', () => {
+  sliderValue.value = sliderElement.noUiSlider.get();
+});
+
+
 export {resetEffects};
+
